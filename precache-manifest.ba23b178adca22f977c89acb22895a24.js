b5a2399c53ae32bc1c641ef2e82e0fd469ff11ae
Hanna Kozak <hannawkozak@gmail.com>
2020-02-12 22:02:50 +0000

    Updates

---
+++ precache-manifest.ba23b178adca22f977c89acb22895a24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6fbaeae561bb018a242cb0a673c136",
+    "url": "/promodoro/index.html"
+  },
+  {
+    "revision": "d2bb2dad14a918bc2397",
+    "url": "/promodoro/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "234264c854adf18d7c5b",
+    "url": "/promodoro/static/js/2.fee1680d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/promodoro/static/js/2.fee1680d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2bb2dad14a918bc2397",
+    "url": "/promodoro/static/js/main.ac7c9d51.chunk.js"
+  },
+  {
+    "revision": "8e3654250d130e7b7fe8",
+    "url": "/promodoro/static/js/runtime-main.146c6189.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/promodoro/static/media/logo.5d5d9eef.svg"
+  }
+]);
